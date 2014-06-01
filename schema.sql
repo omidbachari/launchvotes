c39246c37b7af9218d70b0b76620b9a2f492dfe1
Omid Omid <omidbachari@gmail.com>
2014-06-01 14:58:24 -0400

    Added basic funcitonality - database connection is integrated.

--- schema.sql
+++ schema.sql
@@ -11,3 +11,11 @@ CREATE TABLE nominations (
   nominee_id integer NOT NULL REFERENCES users(id)
 );
 
+
+INSERT INTO users (name) VALUES ('Alex Morgan');
+INSERT INTO users (name) VALUES ('Omid Bachari');
+INSERT INTO users (name) VALUES ('Carol Jones');
+INSERT INTO users (name) VALUES ('Caleb Thill');
+INSERT INTO users (name) VALUES ('Dan Murphy');
+
+INSERT INTO nominations (content, created_at, nominee_id) VALUES ('Worst Face', now(), 1);
--- server.rb
+++ server.rb
@@ -4,14 +4,48 @@ require "pg"
 
 #methods
 
+def db_connection
+  begin
+    connection = PG.connect(dbname: 'launchvotes')
+    yield(connection)
+  ensure
+    connection.close
+  end
+end
+
+def get_award_info
+  connection = PG.connect(dbname: 'launchvotes')
+  award_info = connection.exec('SELECT nominations.content, nominations.created_at, users.name, users.pic_url FROM nominations LEFT JOIN users ON users.id = nominations.nominee_id ORDER BY nominations.created_at')
+  connection.close
+  award_info.to_a
+end
+
+def get_names
+  connection = PG.connect(dbname: 'launchvotes')
+  names = connection.exec('SELECT * FROM users')
+  connection.close
+  names
+end
+
+def add_award_info(nominations_content, nominee_id)
+  connection = PG.connect(dbname: 'launchvotes')
+  sql = "INSERT INTO nominations (content, created_at, nominee_id) VALUES ($1, now(), $2)"
+  db_connection do |conn|
+    conn.exec_params(sql,[nominations_content,nominee_id])
+  end
+end
 
 #http
 
 get '/' do
+  @users = get_names
+  @get_award_info = get_award_info
+
   erb :index
 end
 
 post '/' do
+  add_award_info(params["nominations_content"], params["nominee_id"].to_i)
 
   redirect '/'
 end
