f14eb1bb4b0cc662a312a31b10adecc7f289dd1b
Richard Davis <rdavis.bacs@gmail.com>
2014-10-10 19:50:36 -0400

    use 'expect' syntax

--- spec/factories_spec.rb
+++ spec/factories_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 FactoryGirl.factories.map(&:name).each do |factory_name|
   describe "The #{factory_name} factory" do
      it 'is valid' do
-       FactoryGirl.build(factory_name).should be_valid
+       expect(FactoryGirl.build(factory_name)).to be_valid
      end
   end
 end
