c6f37e582f9e80c8dabf4651ecefe6f5de29036c
Richard Davis <rdavis.bacs@gmail.com>
2015-01-19 19:21:55 -0500

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141114213324) do
+ActiveRecord::Schema.define(version: 20141210203718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,9 @@ ActiveRecord::Schema.define(version: 20141114213324) do
     t.datetime "updated_at"
     t.integer  "votes_count",  default: 0, null: false
   end
+
+  add_index "nominations", ["content", "nominator_id"], name: "index_nominations_on_content_and_nominator_id", unique: true, using: :btree
+  add_index "nominations", ["content", "nominee_id"], name: "index_nominations_on_content_and_nominee_id", unique: true, using: :btree
 
   create_table "users", force: true do |t|
     t.integer  "uid",                             null: false
