76199a34bcc6912e76e91da48ede5ce73f677ae6
Richard Davis <rdavis.bacs@gmail.com>
2015-01-22 04:51:49 -0500

    silence ActiveRecord noise during testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,14 +22,19 @@ include AuthenticationHelper
 
 RSpec.configure do |config|
   config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
   end
 end
 
