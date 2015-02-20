010fb9731c6c8ab7a98f579170e3b4105fbea399
Richard Davis <rdavis.bacs@gmail.com>
2015-02-19 20:05:11 -0500

    create teams

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ files = %w(./db/winter_2014.csv ./db/spring_2015.csv)
 
 files.each do |file|
   team_name = File.basename(files.first, ".csv").titleize
-  team = Team.find_or_initialize_by(name: team_name)
+  team = Team.find_or_create_by(name: team_name)
 
   CSV.foreach(file, headers: true, header_converters: :symbol) do |row|
     user = User.find_or_initialize_by(username: row[:username])
