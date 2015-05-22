69e2a2d588c71c4945f492738962ecb0a74b06fa
Richard Davis <rdavis.bacs@gmail.com>
2015-05-22 14:01:44 -0400

    default team

--- models/team.rb
+++ models/team.rb
@@ -3,4 +3,8 @@ class Team < ActiveRecord::Base
   has_many :users, through: :team_memberships
 
   validates :name, presence: true
+
+  def self.default_team
+    find_or_create_by(name: "Launchers")
+  end
 end
